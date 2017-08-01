e58acf6ba1566dacabdff891196df60722c06852
kikinbmk10 <kikinbmk10@gmail.com>
2017-08-01 10:54:26 -0500

    Modificacion del Pdf

--- app/solicitudes/solicitud.php
+++ app/solicitudes/solicitud.php
@@ -1,37 +1,77 @@
 <?php
 	require_once('../lib/pdf/mpdf.php');
 
+$data = array();
+	foreach ($_POST as $key => $value){
+		$data[] = $value;
+		echo "$key = $value";
+}
 
-	$name = htmlspecialchars($_POST['inNombre']);
-	$empresa = htmlspecialchars($_POST['inEmpresa']);
-	$email = htmlspecialchars($_POST['inEmail']);
-	$tel = htmlspecialchars($_POST['inTel']);
+
+$lista="";
+foreach($_POST as $key => $value){
+   if(strpos($key,"item-done"))
+       $lista.="<li>".$value."</li>";
+}
+
+
+	$empresa = $data[1];
+	$typePage =  $data[4];
+
+
+
+	if (isset($_POST['cb1'])){
+  		$elemnto1 = htmlspecialchars('Caracteristica');
+	}
+
+	if (isset($_POST['cb2'])){
+  		$elemnto2 = htmlspecialchars('Caracteristica2');
+	}
+
+	if (isset($_POST['cb3'])){
+  		$elemnto3 = htmlspecialchars('Caracteristica diferente');
+	}
+
+	if (isset($_POST['cb4'])){
+  		$elemnto4 = htmlspecialchars('Caracteristica ejemplo');
+	}
+
+	if (isset($_POST['cb5'])){
+  		$elemnto5 = htmlspecialchars('CaracteristicaxD');
+	}
+
 	
 	$html = '
 <body>
-	<style>
-
-	</style>
+	
 	<header>
-		 <img id="logo" class="grayscale" src="img/Logo_black.png">
-	</header>';
+		 
+	</header>
+	<div id="divSolicitud">
+			<h1 class="h1Style">Solicitud de cotización para la empresa: '.$empresa.'</h1>
+			
+			<h2 class="h2Style">Objetivo</h2>
+			
+			<p class="pStyle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan ipsum imperdiet, euismod elit sed, tempor lacus. Vestibulum imperdiet tincidunt pulvinar. Nam luctus pulvinar mattis. Etiam consequat tempor nulla, et tristique risus pellentesque vel. Sed aliquam ut dui vitae laoreet. Phasellus aliquam ac urna vitae imperdiet. Etiam sit amet pulvinar diam, ut ornare elit. Sed lobortis est non justo lobortis facilisis. In pellentesque nunc vel posuere consequat. Maecenas sit amet dignissim risus. Nulla facilisi. Duis lacinia placerat tempus. Aliquam ipsum dui, eleifend sed commodo vitae, ultricies ut quam. Curabitur ultricies finibus nisl a tempus. Donec sit amet hendrerit erat, a pulvinar dolor.</p>
+			<div>
+				<h2 class="h2Style">'.$typePage.'</h2>
+				<p class="pList">Características de la página web:</p>
+				<div id="lista">
+					<ul id="listElement">
+						<li>'.$elemnto1.'</li>
+						<li>'.$elemnto2.'</li>
+						<li>'.$elemnto3.'</li>
+						<li>'.$elemnto4.'</li>
+						<li>'.$elemnto5.'</li>
+						'.$lista.'
+						
+					</ul>
+				</div>
 
-$html .='<div id="divSolicitud">
-			<div>
-			'.$name.'
+				<p class="pStyle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan ipsum imperdiet, euismod elit sed, tempor lacus. Vestibulum imperdiet tincidunt pulvinar. Nam luctus pulvinar mattis. Etiam consequat tempor nulla, et tristique risus pellentesque vel. Sed aliquam ut dui vitae laoreet. Phasellus aliquam ac urna vitae imperdiet. Etiam sit amet pulvinar diam, ut ornare elit. Sed lobortis est non justo lobortis facilisis. In pellentesque nunc vel posuere consequat. Maecenas sit amet dignissim risus. Nulla facilisi. Duis lacinia placerat tempus. Aliquam ipsum dui, eleifend sed commodo vitae, ultricies ut quam. Curabitur ultricies finibus nisl a tempus. Donec sit amet hendrerit erat, a pulvinar dolor.</p>
 			</div>
-			<div>
-			'.$empresa.'
-			</div>
-			<div>
-			'.$email.'
-			</div>
-			<div>
-			'.$tel.'
-			</div>
-		</div>';
-
-$html .='
+			
+		</div>
 	<footer>
 		<div id="boxDirection">
 			<div id="line">
@@ -44,11 +84,14 @@ $html .='
 		<div id="correo">
 			<p id="pCorreo">&nbsp;&nbsp;&nbsp;www.aori.mx &nbsp;&nbsp;| &nbsp;&nbsp;hola@aori.mx &nbsp;</p>
 		</div>
-		
 	</footer>
 </body>';
 	
 	$mpdf = new mPDF('c', 'A4', 0,'Montserrat', 0, 0, 0, 0);
+
+	$mpdf->SetDisplayMode('fullpage');
+	$mpdf->SetWatermarkImage('img/cabeza.JPG', 1, '', array( -8, 0));
+	$mpdf->showWatermarkImage = true;
 	$css = file_get_contents('cssSoli/ArchivoPdf.css');
 	$mpdf->writeHTML($css, 1);
 	$mpdf->writeHTML($html);
--- js/addCaracteristica.js
+++ js/addCaracteristica.js
@@ -1,9 +1,9 @@
 function addTodoItem() {
   var todoItem = $("#newItem").val();
   $("#todo-list").append("<li><input type='checkbox'" + 
-                         " name='todo-item-done'" + 
+                         " name='todo-item-done" + todoItem +"'"+
                          " class='todo-item-done'"+ 
-                         " value='" + todoItem + "' /> " +"&nbsp;"+ 
+                         " value='" + todoItem + "' checked/> " +"&nbsp;"+ 
                          todoItem +
                          " <button class='todo-item-delete'>"+
                          "-</button></li>");
@@ -31,6 +31,60 @@ $(function() {
      addTodoItem()
    });
   
+
+
+
+/*method="post" action="http://localhost/quotation/app/solicitudes/solicitud.php"*/
+
+  /*$("#formulario").submit(function(e){
+   e.preventDefault();
+    var nombre = document.getElementById("inNombre").value;
+
+    var empresa = document.getElementById("empresa").value; 
+
+    var correo = document.getElementById("correo").value;
+
+    var telefono = document.getElementById("telefono").value;
+
+    var x = document.getElementById("typePage").selectedIndex;
+    var y = document.getElementById("typePage").options;
+    var tipo= y[x].text;
+
+
+      var array = [];
+
+    $(".todo-item-done:checked").each(function(i, obj){
+      var tmp = obj.value;
+
+      array[i] = tmp
+ 
+      })
+
+      console.log(array)
+
+      $.ajax({
+        type: 'POST',
+        crossDomain: true,
+         dataType : 'text',
+            contentType : 'application/pdf',
+
+        url: 'http://localhost/quotation/app/solicitudes/solicitud.php',
+        data: {
+            'inEmail': 'empresa'
+      },
+      success: function(msg){
+           var blob=new Blob([msg]);
+            var link=document.createElement('a');
+            link.href=window.URL.createObjectURL(blob);
+            link.download="Cotizacion"+new Date()+".pdf";
+            link.click();
+      }
+    });
+    //alert('hola' + nombre)
+  })*/
+
+
+
 //EVENT DELEGATION
 //#todo-list is the event handler because .todo-item-delete doesn't exist when the document loads, it is generated later by a todo entry
 //https://learn.jquery.com/events/event-delegation/
