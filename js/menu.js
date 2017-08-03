31a53bea86a850db691aab9712014839c4323ed2
kikinbmk10 <kikinbmk10@gmail.com>
2017-08-03 17:07:25 -0500

    img nuevas

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,12 @@
-$('.menu-bar').on('click', function(){
-	$('.headerM').toggleClass('abrir');
+
+$(document).ready(function () {
+	
+	$('.menu-bar').on('click', function () {
+		$('.headerM').toggleClass('abrir');
+	});
+
+	$('.btnVerMas').on('click', function () {
+		console.log("object");
+		$('.parrafo3').toggleClass('verMas');
+	});
 });
