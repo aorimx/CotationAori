956b78c0b99f93daeb266dd81fbaa2d56fa639f8
kikinbmk10 <kikinbmk10@gmail.com>
2017-08-09 18:05:04 -0500

    PdfCambios

--- app/solicitudes/solicitud.php
+++ app/solicitudes/solicitud.php
@@ -1,10 +1,10 @@
 <?php
 	require_once('../lib/pdf/mpdf.php');
 
+
 $data = array();
 	foreach ($_POST as $key => $value){
 		$data[] = $value;
-		echo "$key = $value";
 }
 
 
@@ -14,7 +14,7 @@ foreach($_POST as $key => $value){
        $lista.="<li>".$value."</li>";
 }
 
-
+	$nombre =  $data[0];
 	$empresa = $data[1];
 	$typePage =  $data[4];
 
@@ -47,30 +47,44 @@ foreach($_POST as $key => $value){
 	<header>
 		 
 	</header>
+	<div class="fecha">
+		<p class="pFecha">Fecha: '.date("d/m/Y").' &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</p>
+		<p class="pFecha">Lugar: Guadalajara, Jalisco, México.</p>
+		<p class="pNombre">Con Atención a: '.$nombre.'</p>
+		<p class="pNombre">Puesto / Ocupación:</p>
+		<p class="pNombre">Empresa: '.$empresa.'</p>
+	</div>
 	<div id="divSolicitud">
-			<h1 class="h1Style">Solicitud de cotización para la empresa: '.$empresa.'</h1>
-			
-			<h2 class="h2Style">Objetivo</h2>
-			
-			<p class="pStyle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan ipsum imperdiet, euismod elit sed, tempor lacus. Vestibulum imperdiet tincidunt pulvinar. Nam luctus pulvinar mattis. Etiam consequat tempor nulla, et tristique risus pellentesque vel. Sed aliquam ut dui vitae laoreet. Phasellus aliquam ac urna vitae imperdiet. Etiam sit amet pulvinar diam, ut ornare elit. Sed lobortis est non justo lobortis facilisis. In pellentesque nunc vel posuere consequat. Maecenas sit amet dignissim risus. Nulla facilisi. Duis lacinia placerat tempus. Aliquam ipsum dui, eleifend sed commodo vitae, ultricies ut quam. Curabitur ultricies finibus nisl a tempus. Donec sit amet hendrerit erat, a pulvinar dolor.</p>
+			<p class="h1Style">Hola '.$nombre.', </p>
+			<p class="pStyle">A través de la presente cotización agradezco de antemano tu preferencia y me complace comentarte que está dentro de nuestro ADN, el querer colaborar en el desarrollo de plataformas digitales que impulsen la imagen y resultados de profesionistas o empresas en toda Latino América.</p>
 			<div>
-				<h2 class="h2Style">'.$typePage.'</h2>
-				<p class="pList">Características de la página web:</p>
-				<div id="lista">
-					<ul id="listElement">
+				<p class="pList">AORI se especializa en el desarrollo de productos que cuenten con:</p>
+				<div class="lista">
+					<ul class="listElement">
+						<li>Factibilidad Comercial</li>
+						<li>Facilidad de Uso y</li>
+						<li> Factibilidad de Navegación</li>
+					</ul>
+				</div>
+
+				<p class="pCoti">Como profesionista y empresario, comprendo el tedio que significa el disponer de una herramienta digital que no funcione adecuadamente y que genere más problemas que soluciones. Justamente por ello, pongo a tu disposición la siguiente cotización que te brindara tranquilidad y resultados.</p>
+			</div>
+			<p class="pType">'.$typePage.':</p>
+			<div class="lista">
+					<ul class="listElement">
 						<li>'.$elemnto1.'</li>
 						<li>'.$elemnto2.'</li>
 						<li>'.$elemnto3.'</li>
 						<li>'.$elemnto4.'</li>
-						<li>'.$elemnto5.'</li>
 						'.$lista.'
-						
 					</ul>
-				</div>
-
-				<p class="pStyle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan ipsum imperdiet, euismod elit sed, tempor lacus. Vestibulum imperdiet tincidunt pulvinar. Nam luctus pulvinar mattis. Etiam consequat tempor nulla, et tristique risus pellentesque vel. Sed aliquam ut dui vitae laoreet. Phasellus aliquam ac urna vitae imperdiet. Etiam sit amet pulvinar diam, ut ornare elit. Sed lobortis est non justo lobortis facilisis. In pellentesque nunc vel posuere consequat. Maecenas sit amet dignissim risus. Nulla facilisi. Duis lacinia placerat tempus. Aliquam ipsum dui, eleifend sed commodo vitae, ultricies ut quam. Curabitur ultricies finibus nisl a tempus. Donec sit amet hendrerit erat, a pulvinar dolor.</p>
 			</div>
-			
+			<div class="divFirma">
+				<p class="pFirma">Quedo a tus órdenes,</p>
+				<p class="pFirma">A T E N T A M E N T E,</p>
+				<img src="img/Firma.jpg" id="imgFirma">
+				<p class="pDirector">Director General</p>
+			</div>			
 		</div>
 	<footer>
 		<div id="boxDirection">
@@ -78,7 +92,7 @@ foreach($_POST as $key => $value){
 				<hr class="linehr">
 			</div>
 			<div id="txtDirection">
-				<p class"pDir">Victoriano Salado Álvarez #128 L-2, C.P. 44600 Ladrón de Guevara. Guadalajara, Jalisco&nbsp;&nbsp; T.(33) 2306 2020 // (33) 2306 2030</p>
+				<p class="pDir">Victoriano Salado Álvarez #128 L-2, C.P. 44600 Ladrón de Guevara. Guadalajara, Jalisco&nbsp;&nbsp; T.(33) 2306 2020 // (33) 2306 2030</p>
 			</div>
 		</div>
 		<div id="correo">
@@ -86,6 +100,7 @@ foreach($_POST as $key => $value){
 		</div>
 	</footer>
 </body>';
+	
 	
 	$mpdf = new mPDF('c', 'A4', 0,'Montserrat', 0, 0, 0, 0);
 
